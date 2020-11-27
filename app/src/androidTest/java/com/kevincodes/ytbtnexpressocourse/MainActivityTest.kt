46ae81a9f248140334f4f761ec46f203ad54e343
kevingermainbusiness <kevingermainbusiness@gmail.com>
2020-11-26 22:16:08 -0500

    Second commit

--- app/src/androidTest/java/com/kevincodes/ytbtnexpressocourse/MainActivityTest.kt
+++ app/src/androidTest/java/com/kevincodes/ytbtnexpressocourse/MainActivityTest.kt
@@ -4,8 +4,7 @@ import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
@@ -33,5 +32,11 @@ class MainActivityTest {
 
         onView(withId(R.id.tvDisplayText))
             .check(matches(withText("Hello Espresso!")))
+
+        // or you could also check for any views
+        // with the term "Hello Espresso!"
+        // like so
+
+//        onView(withText("Hello Espresso!")).check(matches(isDisplayed()))
     }
 }
